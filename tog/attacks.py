c1b1f04b57cbe3d973cbd18f224f34aeee290bb5
judy12345 <44315906+judy12345@users.noreply.github.com>
2022-05-09 18:47:16 +0800

    Add files via upload

---
+++ tog/attacks.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def tog_vanishing(victim, x_query, n_iter=10, eps=8/255., eps_iter=2/255.):
+    eta = np.random.uniform(-eps, eps, size=x_query.shape)
+    x_adv = np.clip(x_query + eta, 0.0, 1.0)
+    for _ in range(n_iter):
+        grad = victim.compute_object_vanishing_gradient(x_adv)
+        signed_grad = np.sign(grad)
+        x_adv -= eps_iter * signed_grad
+        eta = np.clip(x_adv - x_query, -eps, eps)
+        x_adv = np.clip(x_query + eta, 0.0, 1.0)
+    return x_adv
+
+
+
+
+
+
